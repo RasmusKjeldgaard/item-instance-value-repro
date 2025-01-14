5ff793249cf652c0c6abddf82d23863f105fb19f
RasmusKjeldgaard <rkk@bankdata.dk>
2025-01-14 14:33:00 +0100

    Initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'item-instance-value-repro',
+  webDir: 'www'
+};
+
+export default config;
